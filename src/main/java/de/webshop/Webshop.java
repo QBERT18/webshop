948e163706d186614fcf18f0b3fb7684ae1e8d72
Valeh Mustafayev <valeh.mustafayev@comspace.de>
2020-04-21 21:18:36 +0200

    maven projekt setup

---
+++ src/main/java/de/webshop/Webshop.java
@@ -0,0 +1,12 @@
+package de.webshop;
+
+import javafx.application.Application;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webshop {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
