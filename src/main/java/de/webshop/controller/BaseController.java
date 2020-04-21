9fa4eb39622dc80ec8083a52ed422f67303c69e9
Valeh Mustafayev <valeh.mustafayev@comspace.de>
2020-04-21 23:15:00 +0200

    further project and pom configs

---
+++ src/main/java/de/webshop/controller/BaseController.java
@@ -0,0 +1,13 @@
+package de.webshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BaseController {
+
+    @GetMapping("/")
+    public String test() {
+        return "home";
+    }
+}
