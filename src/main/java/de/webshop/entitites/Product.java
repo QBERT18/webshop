f39cf841e1a5a75bcb4412d86ae88dec690c1ca7
Moritz Stiller <moritz.stiller@gmx.de>
2020-04-25 15:38:31 +0200

    Product and User

---
+++ src/main/java/de/webshop/entitites/Product.java
@@ -0,0 +1,103 @@
+package de.webshop.entitites;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import java.net.URL;
+import java.util.Objects;
+import java.util.Set;
+
+public class Product {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(unique = true)
+    private String productName;
+    private double price; // in EUR
+    private int stock;
+    private Set<URL> imageUrls;
+    private String description;
+
+    public Product(@NotNull String productName, @NotNull double price) {
+        this.productName = productName;
+        this.price = price;
+    }
+
+    public Product() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public Set<URL> getImageUrls() {
+        return imageUrls;
+    }
+
+    public void setImageUrls(Set<URL> imageUrls) {
+        this.imageUrls = imageUrls;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Product product = (Product) o;
+        return id == product.id &&
+                Double.compare(product.price, price) == 0 &&
+                stock == product.stock &&
+                productName.equals(product.productName) &&
+                Objects.equals(imageUrls, product.imageUrls) &&
+                Objects.equals(description, product.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, productName, price, stock, imageUrls, description);
+    }
+}
---
+++ src/main/java/de/webshop/entitites/User.java
@@ -0,0 +1,111 @@
+package de.webshop.entitites;
+
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Objects;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long userID;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String userPermission;
+
+    /**
+     * User Constructor.
+     *
+     * @param email    Email of User.
+     * @param password Hashed Password of User.
+     * @throws NullPointerException When either email, password or displayName is null.
+     */
+    public User(@NotNull final String email, final @NotNull String password) throws NullPointerException {
+        if (email == null || password == null) {
+            throw new NullPointerException();
+        }
+        this.email = email;
+        this.password = password;
+    }
+
+    /**
+     * empty Constructor for JPA
+     */
+    public User() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserPermission() {
+        return userPermission;
+    }
+
+    public void setUserPermission(String userPermission) {
+        this.userPermission = userPermission;
+    }
+
+    public long getUserID() {
+        return userID;
+    }
+
+    public void setUserID(long userID) {
+        this.userID = userID;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return userID == user.userID &&
+                email.equals(user.email) &&
+                password.equals(user.password) &&
+                Objects.equals(firstName, user.firstName) &&
+                Objects.equals(lastName, user.lastName) &&
+                userPermission.equals(user.userPermission);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userID, email, password, firstName, lastName, userPermission);
+    }
+}
