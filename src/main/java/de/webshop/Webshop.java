962ba0c930bdd89ecf810e26c5411253b491f3c5
Valeh Mustafayev <valeh.mustafayev@comspace.de>
2020-04-25 17:42:29 +0200

    main class changes

--- src/main/java/de/webshop/Webshop.java
+++ src/main/java/de/webshop/Webshop.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebshopApplication {
+public class Webshop {
     public static void main(String[] args) {
-        SpringApplication.run(WebshopApplication.class, args);
+        SpringApplication.run(Webshop.class, args);
     }
 }
