2b88b6e9280142189282ee0970ac6979c0c2eb87
Moritz Stiller <moritz.stiller@gmx.de>
2020-05-02 17:26:54 +0200

    rename mail service

--- src/main/java/de/webshop/services/impl/MailServiceImpl.java
+++ src/main/java/de/webshop/services/impl/MailServiceImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.Properties;
 
-@Service("userDbService")
+@Service("mailService")
 public class MailServiceImpl implements MailService {
 
     @Autowired
